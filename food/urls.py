0a8baccf38b4c216cec047fce39c7997c3ed90c2
AndriKls <158137294+AndriKls@users.noreply.github.com>
2024-11-22 14:10:19 +0200

    Added edit and delete item functionalities.

--- food/urls.py
+++ food/urls.py
@@ -6,5 +6,7 @@ app_name = 'food'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>/', views.detail, name='detail'),
-    path('add-item/', views.AddItemView.as_view(), name='add-item')
+    path('add-item/', views.AddItemView.as_view(), name='add-item'),
+    path('update/<int:id>', views.update_item, name='update-item'),
+    path('delete/<int:id>', views.delete_item, name='delete-item'),
 ]
--- food/views.py
+++ food/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Item
 from django.views.generic.edit import FormView
 from .forms import ItemForm
@@ -27,3 +27,24 @@ class AddItemView(FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+def update_item(request, id):
+    item = Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request, 'food/add_item.html', {'form': form})
+
+
+def delete_item(request, id):
+    item = Item.objects.get(id=id)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-delete.html', {'item': item})
